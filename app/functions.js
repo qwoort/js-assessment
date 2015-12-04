6639ff0ce15272ca21304025e1e3d8903b369ce9
Agamyan Michail <Agamyan Michail>
2015-12-04 20:56:44 +0300

    functions is ended

--- app/functions.js
+++ app/functions.js
@@ -1,39 +1,71 @@
 exports = (typeof window === 'undefined') ? global : window;
 
 exports.functionsAnswers = {
-  argsAsArray : function(fn, arr) {
+    argsAsArray: function (fn, arr) {
+        return fn.apply(this, arr);
+    },
 
-  },
+    speak: function (fn, obj) {
+        return fn.bind(obj)();
+    },
 
-  speak : function(fn, obj) {
+    functionFunction: function (str) {
+        return function (newString) {
+            return str + ', ' + newString;
+        }
+    },
 
-  },
+    makeClosures: function (arr, fn) {
+        return arr.map(function (num) {
+            return function () {
+                return fn(num);
+            }
+        });
+    },
 
-  functionFunction : function(str) {
+    partial: function (fn, str1, str2) {
+        return function (str3) {
+            return fn(str1, str2, str3);
+        }
+    },
 
-  },
+    useArguments: function () {
+        var args = Array.prototype.slice.call(arguments);
+        return args.reduce(function (prev, curr) {
+            return prev + curr;
+        })
+    },
 
-  makeClosures : function(arr, fn) {
+    callIt: function (fn) {
+        var args = Array.prototype.slice.call(arguments, 1);
+        return fn.apply(null, args);
+    },
 
-  },
+    partialUsingArguments: function (fn) {
+        var firstArgs = Array.prototype.slice.call(arguments, 1);
 
-  partial : function(fn, str1, str2) {
+        return function () {
+            var secondArgs = Array.prototype.slice.call(arguments);
 
-  },
+            return fn.apply(null, firstArgs.concat(secondArgs));
+        }
+    },
 
-  useArguments : function() {
+    curryIt: function (fn) {
+        var argsNum = fn.length;
+        var args = [];
 
-  },
+        var partialFunc = function (arg) {
+            args.push(arg);
 
-  callIt : function(fn) {
+            if(args.length === argsNum){
+                return fn.apply(this, args);
+            } else {
+                return partialFunc;
+            }
+        }
 
-  },
+        return partialFunc;
 
-  partialUsingArguments : function(fn) {
-
-  },
-
-  curryIt : function(fn) {
-
-  }
+    }
 };
