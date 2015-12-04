f82993e2b97204f33d72a0e1d943b5da06c27eea
Agamyan Michail <Agamyan Michail>
2015-12-04 19:43:45 +0300

    async part is ended

--- app/async.js
+++ app/async.js
@@ -1,11 +1,43 @@
 exports = (typeof window === 'undefined') ? global : window;
 
 exports.asyncAnswers = {
-  async : function(value) {
+    async: function (value) {
 
-  },
+        return new Promise(function (resolve) {
+            resolve(value);
+        });
 
-  manipulateRemoteData : function(url) {
+    },
 
-  }
+    manipulateRemoteData: function (url) {
+
+        return new Promise(function (resolve) {
+            var r = new XMLHttpRequest();
+            r.open('GET', "/data/testdata.json", true);
+            r.send();
+
+            r.onreadystatechange = function () {
+                if (r.readyState !== XMLHttpRequest.DONE) {
+                    return;
+                }
+                if (r.status !== 200) {
+                    return;
+                }
+
+                resolve((function () {
+                    var response = JSON.parse(r.responseText).people;
+
+                    return response
+                        .map(function (person) {
+                            return person.name;
+                        })
+                        .sort()
+                    ;
+                    
+                })());
+            };
+
+        });
+
+    }
 };
