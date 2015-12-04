be5b047e70a0223cbac95ded90ea194873af5a2b
Agamyan Michail <Agamyan Michail>
2015-12-04 21:29:29 +0300

    numbers is ended

--- app/numbers.js
+++ app/numbers.js
@@ -1,19 +1,27 @@
 exports = (typeof window === 'undefined') ? global : window;
 
 exports.numbersAnswers = {
-  valueAtBit: function(num, bit) {
+    valueAtBit: function (num, bit) {
+        var bitView = new Number(num).toString(2);
+        var digit = bitView[bitView.length - bit];
+        return +digit;
+    },
 
-  },
+    base10: function (str) {
+        return +new Number('0b'+str);
+    },
 
-  base10: function(str) {
+    convertToBinary: function (num) {
+        var computedString = new Number(num).toString(2);
 
-  },
+        var zeros = '';
+        for(var i = computedString.length; i < 8; i++){
+            zeros += '0';
+        }
+        return zeros + (new Number(num).toString(2));
+    },
 
-  convertToBinary: function(num) {
+    multiply: function (a, b) {
 
-  },
-
-  multiply: function(a, b) {
-
-  }
+    }
 };
