7003abbbc80711ba0f62cf2ed9441eed750134b5
Agamyan Michail <Agamyan Michail>
2015-12-04 21:34:41 +0300

    objects is ended

--- app/objects.js
+++ app/objects.js
@@ -1,15 +1,21 @@
 exports = (typeof window === 'undefined') ? global : window;
 
-exports.objectsAnswers =  {
-  alterContext : function(fn, obj) {
+exports.objectsAnswers = {
+    alterContext: function (fn, obj) {
+        return fn.call(obj);
+    },
 
-  },
+    alterObjects: function (constructor, greeting) {
+        constructor.prototype.greeting = greeting;
+    },
 
-  alterObjects : function(constructor, greeting) {
-
-  },
-
-  iterate : function(obj) {
-
-  }
+    iterate: function (obj) {
+        var buffer = [];
+        for(var i in obj){
+            if(obj.hasOwnProperty(i)){
+                buffer.push(i+': '+obj[i]);
+            }
+        }
+        return buffer;
+    }
 };
