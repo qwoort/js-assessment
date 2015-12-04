f43ee6df6d582ba36c3b4c70d6f539d23de7e86d
Agamyan Michail <Agamyan Michail>
2015-12-04 21:03:28 +0300

    functions is ended

--- app/logicalOperators.js
+++ app/logicalOperators.js
@@ -1,11 +1,27 @@
 exports = (typeof window === 'undefined') ? global : window;
 
 exports.logicalOperatorsAnswers = {
-  or : function(a, b) {
+    or: function (a, b) {
+        if(a == true){
+            return a;
 
-  },
+        } else if (b == true) {
+            return b;
 
-  and : function(a, b) {
+        } else {
+            return false;
+        }
+    },
 
-  }
+    and: function (a, b) {
+        if(a == false){
+            return a;
+
+        } else if (b == false) {
+            return b;
+
+        } else {
+            return true;
+        }
+    }
 };
